df4e19efac8df6bfc2d7375249023b3a3ddd8289
Luzefiru <ian.deejay.iv@gmail.com>
2025-01-09 14:24:31 +0800

    feat: add logger utility, global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+import { env } from "./config/env.config"
+import { Logger } from "./utils/logger.util"
+import { swagger } from "@elysiajs/swagger"
 import { Elysia } from "elysia"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000)
+class Note {
+  constructor(public data: string[] = ["Moonhalo"]) {}
+}
+
+const app = new Elysia()
+  .use(swagger())
+  .onError(({ error, code }) => {
+    Logger.error("The server encountered an error", error)
+
+    switch (code) {
+      case "NOT_FOUND":
+        return "Not found"
+      default:
+        return "An unexpected error occured"
+    }
+  })
+  .decorate("note", new Note())
+  .get("/error", () => {
+    throw new Error("An error")
+  })
+  .get("/note", ({ note }) => note.data)
+  .listen(env.PORT)
 
 console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
