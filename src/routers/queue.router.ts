d80dc912cf01f3411eb56d488967bc35203128a2
Samuel-Bonghanoy <20103261@usc.edu.ph>
2025-01-10 14:39:57 +0800

    feat(admin): implement basic auth

--- src/routers/queue.router.ts
+++ src/routers/queue.router.ts
@@ -1,8 +1,15 @@
 import { queueNumberService } from "../db/services/queue-number.service"
 import { CourseUnion } from "../types/entities/dtos/CourseUnion"
+import { basicAuth } from "@eelkevdbos/elysia-basic-auth"
 import Elysia, { t } from "elysia"
 
 export const queue = new Elysia({ prefix: "/queue" })
+  .use(
+    basicAuth({
+      credentials: { env: "ADMIN_CREDENTIALS" },
+      scope: "/admin",
+    }),
+  )
   .model({
     course: t.Object({
       course: CourseUnion,
@@ -17,10 +24,10 @@ export const queue = new Elysia({ prefix: "/queue" })
   .get("/:course/number/current", async ({ params: { course } }) => {
     return await queueNumberService.findCurrentQueueByCourse(course)
   })
-  .patch("/:course/number/current", async ({ params: { course } }) => {
+  .patch("/admin/:course/number/current", async ({ params: { course } }) => {
     return await queueNumberService.dequeue(course)
   })
-  .delete("/:course/reset", ({ params: { course } }) => {
+  .delete("/admin/:course/reset", ({ params: { course } }) => {
     return queueNumberService.resetByCourse(course)
   })
-  .delete("/reset", () => queueNumberService.resetAll())
+  .delete("/admin/reset", () => queueNumberService.resetAll())
