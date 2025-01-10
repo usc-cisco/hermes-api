aa64a0fe142dc634b1f42cc6e1ae8432d46d2bda
Samuel-Bonghanoy <20103261@usc.edu.ph>
2025-01-10 18:33:35 +0800

    fix: fix basic auth not working

--- src/routers/queue.router.ts
+++ src/routers/queue.router.ts
@@ -1,15 +1,39 @@
+// import { queueNumberService } from "../db/services/queue-number.service"
+// import { CourseUnion } from "../types/entities/dtos/CourseUnion"
+// // import { basicAuth } from "@eelkevdbos/elysia-basic-auth"
+// import Elysia, { t } from "elysia"
+// export const queue = new Elysia({ prefix: "/queue" })
+//   .model({
+//     course: t.Object({
+//       course: CourseUnion,
+//     }),
+//   })
+//   .guard({
+//     params: "course",
+//   })
+//   .post("/:course/number", async ({ params: { course } }) => {
+//     try {
+//       return await queueNumberService.enqueue(course)
+//     } catch (error) {
+//       console.error("Enqueue error:", error)
+//       throw error
+//     }
+//   })
+//   .get("/:course/number/current", async ({ params: { course } }) => {
+//     return await queueNumberService.findCurrentQueueByCourse(course)
+//   })
+//   .patch("/admin/:course/number/current", async ({ params: { course } }) => {
+//     return await queueNumberService.dequeue(course)
+//   })
+//   .delete("/admin/:course/reset", ({ params: { course } }) => {
+//     return queueNumberService.resetByCourse(course)
+//   })
+//   .delete("/admin/reset", () => queueNumberService.resetAll())
 import { queueNumberService } from "../db/services/queue-number.service"
 import { CourseUnion } from "../types/entities/dtos/CourseUnion"
-import { basicAuth } from "@eelkevdbos/elysia-basic-auth"
 import Elysia, { t } from "elysia"
 
 export const queue = new Elysia({ prefix: "/queue" })
-  .use(
-    basicAuth({
-      credentials: { env: "ADMIN_CREDENTIALS" },
-      scope: "/admin",
-    }),
-  )
   .model({
     course: t.Object({
       course: CourseUnion,
@@ -24,10 +48,10 @@ export const queue = new Elysia({ prefix: "/queue" })
   .get("/:course/number/current", async ({ params: { course } }) => {
     return await queueNumberService.findCurrentQueueByCourse(course)
   })
-  .patch("/admin/:course/number/current", async ({ params: { course } }) => {
+  .patch("/:course/number/current", async ({ params: { course } }) => {
     return await queueNumberService.dequeue(course)
   })
-  .delete("/admin/:course/reset", ({ params: { course } }) => {
+  .delete("/:course/reset", ({ params: { course } }) => {
     return queueNumberService.resetByCourse(course)
   })
-  .delete("/admin/reset", () => queueNumberService.resetAll())
+  .delete("/reset", () => queueNumberService.resetAll())
