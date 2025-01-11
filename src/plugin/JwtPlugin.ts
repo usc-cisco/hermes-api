0b9bded9ad251b8a9de456401a93825c9730ed19
Luzefiru <ian.deejay.iv@gmail.com>
2025-01-11 09:29:33 +0800

    refactor: move variables to env config

--- src/plugin/JwtPlugin.ts
+++ src/plugin/JwtPlugin.ts
@@ -1,17 +1,15 @@
+import { env } from "../config/env.config"
 import { JWTModel } from "../types/entities/dtos/JwtModel"
 import { jwt } from "@elysiajs/jwt"
 import { Elysia } from "elysia"
-
-// Expires on EOD time - curr time
-const JWTExpiry = Number(process.env.QUEUE_EXPIRY) - new Date().getHours()
 
 export const jwtPlugin = new Elysia()
   .use(
     jwt({
       name: "queueJwt",
       schema: JWTModel,
-      secret: process.env.JWT_SECRET || "TEST SECRET KEY",
-      // exp: JWTExpiry + "h", // It expires 5pm+
+      secret: env.JWT_SECRET,
+      // exp: `${env.JWT_EXPIRY}h`, // It expires 5pm+
       exp: "1h", // testing
     }),
   )
