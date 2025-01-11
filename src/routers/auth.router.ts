00e49066fa1305d6385917bb2834bed4eb88ef2c
Samuel-Bonghanoy <20103261@usc.edu.ph>
2025-01-11 23:58:59 +0800

    feat(admin): create login endpoint for admin

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -3,16 +3,81 @@ import { jwtPlugin } from "../plugin/JwtPlugin"
 import { getAuthToken } from "../services/auth.service"
 import { JwtModel } from "../types/entities/dtos/JwtModel"
 import { HttpStatusEnum } from "../types/enums/HttpStatusEnum"
+import { AuthMiddlewareContext, QueueJwtPayload } from "../types/interfaces/JwtInterface"
+import { basicAuth } from "@eelkevdbos/elysia-basic-auth"
 import Elysia from "elysia"
 
 export const auth = new Elysia({ prefix: "/auth" })
+  .use(
+    basicAuth({
+      credentials: { env: "ADMIN_CREDENTIALS" },
+      scope: "/auth/admin",
+    }),
+  )
   .model({
     JwtModel,
   })
   .use(jwtPlugin)
   .post(
+    "/admin/sign-in",
+    ({ set, headers }: AuthMiddlewareContext) => {
+      const { authorization } = headers
+
+      if (!authorization || !authorization.startsWith("Basic ")) {
+        set.status = HttpStatusEnum.UNAUTHORIZED
+        return { message: "Missing or invalid authorization header" }
+      }
+
+      const basicAuthToken = authorization.split(" ")[1]
+
+      return {
+        token: basicAuthToken,
+      }
+    },
+    {
+      detail: {
+        tags: ["Auth"],
+        description: "Use this to validate an admin sign in",
+        responses: {
+          "201": {
+            description: "Admin successfully logged in",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    message: {
+                      type: "string",
+                      description: "Error message describing the issue.",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          "400": {
+            description: "Bad request. Missing or invalid parameters, or invalid credentials.",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    message: {
+                      type: "string",
+                      description: "Error message describing the issue.",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  )
+  .post(
     "/sign-in",
-    async ({ set, queueJwt, body }) => {
+    async ({ set, queueJwt, body }: AuthMiddlewareContext) => {
       if (!body.idNumber || !body.course) {
         set.status = HttpStatusEnum.BAD_REQUEST
         return { message: "ID number and course are required" }
@@ -26,7 +91,7 @@ export const auth = new Elysia({ prefix: "/auth" })
       }
 
       set.status = HttpStatusEnum.CREATED
-      const token = await getAuthToken(queueJwt, body)
+      const token = await getAuthToken(queueJwt, body as QueueJwtPayload)
       return {
         token,
       }
