b4ea4a2da8232f632fae7ed94544d0fef6a65883
Luzefiru <ian.deejay.iv@gmail.com>
2025-01-12 17:10:25 +0800

    chore: temporarily remove expiry for dev

--- src/plugin/JwtPlugin.ts
+++ src/plugin/JwtPlugin.ts
@@ -3,15 +3,14 @@ import { JwtModel } from "../types/entities/dtos/JwtModel"
 import { jwt } from "@elysiajs/jwt"
 import { Elysia } from "elysia"
 
-
 export const jwtPlugin = new Elysia()
   .use(
     jwt({
       name: "queueJwt",
       schema: JwtModel,
       secret: env.JWT_SECRET,
-      exp: `${env.JWT_EXPIRY}s`, // It expires 5pm+
-      // exp: "1h", // testing
+      // exp: `${env.JWT_EXPIRY}s`, // It expires 5pm+
+      exp: "1h", // testing
     }),
   )
   .derive({ as: "global" }, ({ queueJwt }) => {
