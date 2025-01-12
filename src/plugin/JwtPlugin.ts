ce9e0c11dc08c8566c7bc7344b1fb9bcf85a4088
23000003 <kurtdicdican@gmail.com>
2025-01-12 16:17:14 +0800

    refactor: jwt-expiry plugin & time util

--- src/plugin/JwtPlugin.ts
+++ src/plugin/JwtPlugin.ts
@@ -3,14 +3,15 @@ import { JwtModel } from "../types/entities/dtos/JwtModel"
 import { jwt } from "@elysiajs/jwt"
 import { Elysia } from "elysia"
 
+
 export const jwtPlugin = new Elysia()
   .use(
     jwt({
       name: "queueJwt",
       schema: JwtModel,
       secret: env.JWT_SECRET,
-      // exp: `${env.JWT_EXPIRY}h`, // It expires 5pm+
-      exp: "1h", // testing
+      exp: `${env.JWT_EXPIRY}s`, // It expires 5pm+
+      // exp: "1h", // testing
     }),
   )
   .derive({ as: "global" }, ({ queueJwt }) => {
