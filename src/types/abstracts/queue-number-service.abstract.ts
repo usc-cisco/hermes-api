f0d33c7913f0427eada3a5d4d8d2747a94866fd2
Luzefiru <ian.deejay.iv@gmail.com>
2025-01-13 23:48:16 +0800

    feat: add studentId to current queue number GET route

--- src/types/abstracts/queue-number-service.abstract.ts
+++ src/types/abstracts/queue-number-service.abstract.ts
@@ -4,7 +4,9 @@ import { CourseNameEnum } from "../enums/CourseNameEnum"
 export type IQueueNumberService = {
   findByCourse(courseName: CourseNameEnum): Promise<Partial<QueueNumber[]>>
   findByStudentId(studentId: string): Promise<QueueNumber>
-  findCurrentQueueByCourse(courseName: CourseNameEnum): Promise<{ current: number; max: number }>
+  findCurrentQueueByCourse(
+    courseName: CourseNameEnum,
+  ): Promise<{ current: number; max: number; currentStudentId: string | null }>
   enqueue(courseName: CourseNameEnum, studentId: string): Promise<QueueNumber>
   dequeueFront(courseName: CourseNameEnum): Promise<void>
   dequeueById(studentId: string): Promise<void>
