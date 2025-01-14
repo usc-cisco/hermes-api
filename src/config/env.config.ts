3f44e938771265b52353ae8c6eabb49ab262897c
23000003 <kurtdicdican@gmail.com>
2025-01-14 14:47:17 +0800

    nit: update jwt start time

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,5 +4,5 @@ export const env = {
   PORT: process.env.PORT || 3000,
   LOGFILE_NAME: process.env.LOGFILE_NAME || "./server.log",
   JWT_SECRET: process.env.JWT_SECRET || "TEST SECRET KEY",
-  JWT_EXPIRY: getJwtExpiry("07:30", "23:59"),
+  JWT_EXPIRY: getJwtExpiry("08:00", "23:59"),
 }
